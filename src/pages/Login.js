f5a84303e3f61f3514d65de16f7d60025c1615c4
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-28 17:08:06 +0500

    ternary operator

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -77,71 +77,81 @@ export const Login = () => {
   return (
     <div className="login d-flex justify-content-center align-items-center">
       <div className="container">
-        <div className="row">
-          <div className="col">
-            <h2 className="text-white text-center">User Email:{user.email}</h2>
-            <h2>User UID:{user.uid}</h2>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-12 col-md-6 offset-md-3 col-lg-4 offset-lg-4 ">
-            <div className="card p-3 p-md-4 shadow-lg p-3 mb-5 bg-body rounded">
-              <div className="row">
-                <div className="col">
-                  <h1 className="text-center mb-5">SignUp</h1>
-                </div>
-                <form onSubmit={handleSubmit}>
-                  <div className="row mb-3">
-                    <div className="col">
-                      <input
-                        type="email"
-                        placeholder="Email"
-                        name="email"
-                        className="form-control"
-                        onChange={handleChange}
 
-                      />
-                    </div>
-                  </div>
-
-                  <div className="row mb-3 mt-2">
-                    <div className="col">
-                      <input
-                        type="Password"
-                        name="password"
-                        placeholder="Password"
-                        className="form-control"
-                        onChange={handleChange}
-
-                      />
-                    </div>
-                  </div>
+        {
+          user.email
+            ?
+            <div className="row">
+              <div className="col">
+                <h2 className="text-white text-center">User Email:{user.email}</h2>
+                <h2>User UID:{user.uid}</h2>
+              </div>
+            </div>
+            :
+            <div className="row">
+              <div className="col-sm-12 col-md-6 offset-md-3 col-lg-4 offset-lg-4 ">
+                <div className="card p-3 p-md-4 shadow-lg p-3 mb-5 bg-body rounded">
                   <div className="row">
                     <div className="col">
-                      <button className="btn btn-primary w-100">Register Form </button>
+                      <h1 className="text-center mb-5">SignUp</h1>
                     </div>
-                  </div>
-                </form>
-                <div className="row">
-                  <div className="col">
-                    {/* {buttons.map((button, i) => {
-                      console.log(i,button);
-                      return (
-                        <button
-                          key={i}
-                          className="btn rounded-circle text-white me-3 mt-3 ms-2 "
-                          style={{ background: button.color }}
-                        >
-                          <i className={`bi bi-${button.icon}`}></i>
-                        </button>
-                      );
-                    })} */}
+                    <form onSubmit={handleSubmit}>
+                      <div className="row mb-3">
+                        <div className="col">
+                          <input
+                            type="email"
+                            placeholder="Email"
+                            name="email"
+                            className="form-control"
+                            onChange={handleChange}
+
+                          />
+                        </div>
+                      </div>
+
+                      <div className="row mb-3 mt-2">
+                        <div className="col">
+                          <input
+                            type="Password"
+                            name="password"
+                            placeholder="Password"
+                            className="form-control"
+                            onChange={handleChange}
+
+                          />
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col">
+                          <button className="btn btn-primary w-100">Register Form </button>
+                        </div>
+                      </div>
+                    </form>
+                    <div className="row">
+                      <div className="col">
+                        {/* {buttons.map((button, i) => {
+                    console.log(i,button);
+                    return (
+                      <button
+                        key={i}
+                        className="btn rounded-circle text-white me-3 mt-3 ms-2 "
+                        style={{ background: button.color }}
+                      >
+                        <i className={`bi bi-${button.icon}`}></i>
+                      </button>
+                    );
+                  })} */}
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
+
+
+        }
+
+
       </div>
     </div>
   );
