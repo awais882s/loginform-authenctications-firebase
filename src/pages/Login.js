1915eb081046b85d41c67c9db0cfac01729b2f0a
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-28 16:43:38 +0500

    use Effect

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,98 +1,120 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../pages/index.css";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 import { auth } from "../config/firebase";
 // for firbase
 const initialState = { email: "", password: "" };
 // firebase authentications
-
 export const Login = () => {
-  const [data, setdata] = useState(initialState);
 
-  // for firebase
-  const handleChange = (e) => {
-    setdata({
-      ...data, [e.target.name]: e.target.value,
-
-    })
-    console.log(e.target.name);
-  }
-
-
-
-  // const buttons = [
-  //   { color: "#1877f2", icon: "facebook" },
-  //   { color: "#25d366", icon: "whatsapp" },
-  //   { color: "#dd4b39", icon: "google plus" },
-  //   { color: "#1da1f2", icon: "twitter" },
-  // ];
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    //destructuring use below
-    const { email, password } = data;
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        console.log("Registered successfully");
-        console.log(userCredential);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
         console.log(user);
-
         // ...
+      } else {
+        // User is signed out
+        // ...
+      }
+
+
+    }, []);
+
+
+
+
+
+
+    const [data, setdata] = useState(initialState);
+
+    // for firebase
+    const handleChange = (e) => {
+      setdata({
+        ...data, [e.target.name]: e.target.value,
+
       })
-      .catch((error) => {
-        // const errorCode = error.code;
-        // const errorMessage = error.message;
-        console.log(error);
-        // ..
-      });
-  };
-  return (
-    <div className="login d-flex justify-content-center align-items-center">
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12 col-md-6 offset-md-3 col-lg-4 offset-lg-4 ">
-            <div className="card p-3 p-md-4 shadow-lg p-3 mb-5 bg-body rounded">
-              <div className="row">
-                <div className="col">
-                  <h1 className="text-center mb-5">SignUp</h1>
-                </div>
-                <form onSubmit={handleSubmit}>
-                  <div className="row mb-3">
-                    <div className="col">
-                      <input
-                        type="email"
-                        placeholder="Email"
-                        name="email"
-                        className="form-control"
-                        onChange={handleChange}
+      console.log(e.target.name);
+    }
 
-                      />
-                    </div>
-                  </div>
 
-                  <div className="row mb-3 mt-2">
-                    <div className="col">
-                      <input
-                        type="Password"
-                        name="password"
-                        placeholder="Password"
-                        className="form-control"
-                        onChange={handleChange}
 
-                      />
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col">
-                      <button className="btn btn-primary w-100">Register Form </button>
-                    </div>
-                  </div>
-                </form>
+    // const buttons = [
+    //   { color: "#1877f2", icon: "facebook" },
+    //   { color: "#25d366", icon: "whatsapp" },
+    //   { color: "#dd4b39", icon: "google plus" },
+    //   { color: "#1da1f2", icon: "twitter" },
+    // ];
+
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      //destructuring use below
+      const { email, password } = data;
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          console.log("user Logged In ");
+          console.log(userCredential);
+          console.log(user);
+
+          // ...
+        })
+        .catch((error) => {
+          // const errorCode = error.code;
+          // const errorMessage = error.message;
+          console.log(error);
+          // ..
+        });
+    };
+    return (
+      <div className="login d-flex justify-content-center align-items-center">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 col-md-6 offset-md-3 col-lg-4 offset-lg-4 ">
+              <div className="card p-3 p-md-4 shadow-lg p-3 mb-5 bg-body rounded">
                 <div className="row">
                   <div className="col">
-                    {/* {buttons.map((button, i) => {
+                    <h1 className="text-center mb-5">SignUp</h1>
+                  </div>
+                  <form onSubmit={handleSubmit}>
+                    <div className="row mb-3">
+                      <div className="col">
+                        <input
+                          type="email"
+                          placeholder="Email"
+                          name="email"
+                          className="form-control"
+                          onChange={handleChange}
+
+                        />
+                      </div>
+                    </div>
+
+                    <div className="row mb-3 mt-2">
+                      <div className="col">
+                        <input
+                          type="Password"
+                          name="password"
+                          placeholder="Password"
+                          className="form-control"
+                          onChange={handleChange}
+
+                        />
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col">
+                        <button className="btn btn-primary w-100">Register Form </button>
+                      </div>
+                    </div>
+                  </form>
+                  <div className="row">
+                    <div className="col">
+                      {/* {buttons.map((button, i) => {
                       console.log(i,button);
                       return (
                         <button
@@ -104,6 +126,7 @@ export const Login = () => {
                         </button>
                       );
                     })} */}
+                    </div>
                   </div>
                 </div>
               </div>
@@ -111,6 +134,5 @@ export const Login = () => {
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
