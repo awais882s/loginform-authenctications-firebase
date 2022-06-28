4ef2b553012e0eaafa9dc74b5606675a4de43bce
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-28 15:26:23 +0500

    import get auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { Login } from "./pages/Login";
 import "bootstrap/dist/js/bootstrap.min.js"
 import "./scss/App.scss";
+// firebase authentications
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth } from "../src/config/firebase";
 
 function App() {
   return (
