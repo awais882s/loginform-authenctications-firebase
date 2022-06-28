c684de95c048fc321621b8891d22af6545dca49b
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-28 14:34:00 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Login } from "./pages/Login";
+import "bootstrap/dist/js/bootstrap.min.js"
+import "./scss/App.scss";
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
